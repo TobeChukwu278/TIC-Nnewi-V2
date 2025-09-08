b5cbf7daec0e25756a546ff79fff616af508deaf
Tobe Chukwu <111694922+TobeChukwu278@users.noreply.github.com>
2025-09-08 10:19:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ import path from 'path'
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   preview: {
-  allowedHosts: [
-    'tic-nnewi-v2.onrender.com',
-    'tic-nnewi-v2.vercel.app'
-  ],
-  port: process.env.PORT || 4173,
-  host: true
-  }
+    allowedHosts: [
+      'tic-nnewi-v2.onrender.com',
+      'tic-nnewi-v2.vercel.app'
+    ],
+    port: process.env.PORT || 4173,
+    host: true
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
